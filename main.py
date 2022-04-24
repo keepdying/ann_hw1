091db6717be9b3659ee89c6acdeb6c842b4ce97a
keepdying <yunusemre34tr@gmail.com>
2022-04-24 21:38:58 +0300

    added biasless option

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ from tqdm import tqdm
 
 class Perceptron:
 
-    def __init__(self, num_iter=10, const=0.1, verbose=False):
+    def __init__(self, num_iter=10, const=0.1, verbose=False, bias=True):
         self.const = const
         self.num_iter = num_iter
         self.errors = []
         self.verbose = verbose
+        self.bias = bias
+        self.__bias = 0
+
 
     def train(self, X, y, weight=None):
         input_count = X.shape[0]
@@ -15,16 +18,14 @@ class Perceptron:
         if weight is None:
             self.w = np.random.rand((X.shape[1] + 1))
         else: self.w = weight
-        self.bias = 1
-
+        
         for iterr in range(self.num_iter):
             
             error = 0
             for index in tqdm(range(input_count)):
                 update = self.const * (y[index] - self.predict(X[index]))
                 self.w += update * X[index]
-                self.bias += update * 1
-
+                if self.bias: self.__bias += update * 1
                 if update != 0.0: error += 1
 
             self.errors.append(error)
@@ -39,18 +40,18 @@ class Perceptron:
         return self
 
     def predict(self, X):
-        return self.__activation(np.dot(X, self.w) + self.bias)
+        return self.__activation(np.dot(X, self.w) + self.__bias)
 
     def __activation(self, x):
         return np.where(x >= 0.0, 1, -1)
 
 def main():
-    unit = Perceptron(num_iter= 10, const= 0.1, verbose= True)
+    unit = Perceptron(num_iter=10, const= 0.1, verbose=True, bias=False)
     weights = np.array([1, 0], dtype='float64')
     data_x = np.array([[1, -1], [0, 1], [0.5, -1]])
     data_y = np.array([-1, 1, -1])
 
-    unit.train(X= data_x, y= data_y, weight= weights)
+    unit.train(X=data_x, y=data_y, weight= weights)
     print(unit.w)
 if __name__ == "__main__":
     main()
